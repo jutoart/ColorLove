a272ab4059caf950d7a14b9e7566f6c85ce55085
Art Huang <jutoart@gmail.com>
2025-08-17 15:34:19 +0800

    Initial Commit

---
+++ ColorLove/ColorLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLoveApp.swift
+//  ColorLove
+//
+//  Created by Art Huang on 2025/8/17.
+//
+
+import SwiftUI
+
+@main
+struct ColorLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

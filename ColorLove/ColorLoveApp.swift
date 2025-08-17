893ed138ec29791e427de8df438bad0cc33e3b7f
Art Huang <jutoart@gmail.com>
2025-08-17 15:35:13 +0800

    Project setup

--- ColorLove/ColorLoveApp.swift
+++ ColorLove/ColorLoveApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct ColorLoveApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      EmptyView()
     }
+  }
 }
